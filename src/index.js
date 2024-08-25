9a6229170147e376e62c997b99ebdab6e6984b9b
Alysson Batista de Souza <alyssonbatista241@gmail.com>
2024-08-25 07:57:50 -0300

    library updates and bug fixes

--- src/index.js
+++ src/index.js
@@ -1,26 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './reset.css';
-
-
-import { BrowserRouter , Switch, Route }  from 'react-router-dom';
-import CadastroVideo from './pages/cadastro/Video'
-import CadastroCategoria from './pages/cadastro/Categoria'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
 import Home from './pages/Home';
 
-const Pagina404 = () => (<div>erro 404!</div> )
+const Pagina404 = () => (<div>erro 404!</div>);
 
 ReactDOM.render(
-   <BrowserRouter>
-      <Switch>
-        <Route path="/" component={Home} exact />
-        <Route path="/cadastro/video" component={CadastroVideo} />
-        <Route path="/cadastro/categoria" component={CadastroCategoria} />
-        <Route component={Pagina404} />
-      </Switch>
-      
-   </BrowserRouter>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/cadastro/video" element={<CadastroVideo />} />
+      <Route path="/cadastro/categoria" element={<CadastroCategoria />} />
+      <Route path="*" element={<Pagina404 />} />
+    </Routes>
+  </BrowserRouter>,
   document.getElementById('root')
 );
-
- 
